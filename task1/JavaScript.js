8e4b10e4144357426b644f6a81ad8e8faa9aa9ba
zeinabrefat <zeinabrefat@yahoo.com>
2020-10-14 16:04:11 +0200

    add

---
+++ task1/JavaScript.js
@@ -0,0 +1,6 @@
+﻿function addNumbers() {
+    var val1 = parseInt(document.getElementById("aa").value);
+    var val2 = parseInt(document.getElementById("bb").value);
+    var ansD = document.getElementById("answer");
+    ansD.value = val1 + val2;
+}
