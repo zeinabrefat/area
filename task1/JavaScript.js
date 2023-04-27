36376eff3925730fef7cd0f285fb8d65fcb29ad7
zeinabrefat <68854255+zeinabrefat@users.noreply.github.com>
2023-04-27 20:20:04 +0200

    Delete task1 directory

--- task1/JavaScript.js
+++
@@ -1,6 +0,0 @@
-﻿function addNumbers() {
-    var val1 = parseInt(document.getElementById("aa").value);
-    var val2 = parseInt(document.getElementById("bb").value);
-    var ansD = document.getElementById("answer");
-    ansD.value = val1 + val2;
-}
